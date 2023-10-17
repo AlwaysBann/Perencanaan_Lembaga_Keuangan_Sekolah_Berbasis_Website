ccd09cf26ec6df98f173bd7d6a1e5434064eb4ea
AlwaysBann <subkhanravi@gmail.com>
2023-10-17 23:20:53 +0700

    Membuat project laravel

---
+++ app/Http/Controllers/RuanganController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ruangan;
+use Illuminate\Http\Request;
+
+class RuanganController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ruangan $ruangan)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ruangan $ruangan)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, ruangan $ruangan)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ruangan $ruangan)
+    {
+        //
+    }
+}
