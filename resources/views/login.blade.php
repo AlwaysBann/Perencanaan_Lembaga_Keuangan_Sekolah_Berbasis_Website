9eaaf59e1288169a00a1f788ec402aae3bd254c1
AlwaysBann <subkhanravi@gmail.com>
2023-11-24 13:33:43 +0700

    menambah fitur pada login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -17,15 +17,21 @@
     <div class="row justify-content-center align-items-center rounded-3 p-4" style="border: 3px solid #E6B31E; background-color: #F8F9FA; max-width: 400px;">
       <img src="{{asset('img/logo.png')}}" style="width: 120px; margin-bottom: 50px; margin-top: 20px" alt="">
       <h1 class="align-items-center " style="text-align: center; font-weight: 600">Login</h1>
-    <form method="POST" action="">
+    <form method="POST" action="" class="needs-validation" novalidate>
       @csrf
         <div class="form-group mb-3">
           <label for="username" style="">Username</label>
           <input type="text" class="form-control" id="username" name="username" placeholder="Username" required>
+        <div class="invalid-feedback" style="font-size: 15px; color: red; font-weight: 700;">
+            Username wajib diisi!
+        </div>
         </div>
         <div class="form-group">
           <label for="password">Password</label>
           <input type="password" class="form-control" id="password" name="password" placeholder="Password" required>
+          <div class="invalid-feedback" style="font-size: 15px; color: red; font-weight: 700;">
+            Password wajib diisi!
+        </div>
         </div>
         <button type="submit" class="btn my-3 justify-content-center align-items-center" style="color: #E6B31E; background: #343434; padding-left: 100px; padding-right:100px; margin-left: 40px">login</button>
       </form>
@@ -33,3 +39,25 @@
     </div>
 </body>
 </html>
+<script>
+  // Example starter JavaScript for disabling form submissions if there are invalid fields
+      (function () {
+        'use strict'
+      
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.querySelectorAll('.needs-validation')
+      
+        // Loop over them and prevent submission
+        Array.prototype.slice.call(forms)
+          .forEach(function (form) {
+            form.addEventListener('submit', function (event) {
+              if (!form.checkValidity()) {
+                event.preventDefault()
+                event.stopPropagation()
+              }
+      
+              form.classList.add('was-validated')
+            }, false)
+          })
+      })()
+  </script>
